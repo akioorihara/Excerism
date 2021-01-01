05746e2b8c20f6ca4b9fdd6a037f6896b7440acd
akioorihara <56176776+akioorihara@users.noreply.github.com>
2020-12-31 22:16:07 -0600

    This solves this problem
    but there is still an error about this exercise

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,9 +1,9 @@
-def two_fer(name):
+
+
+def two_fer(name=''):
+    if name == '': 
+        name = "you"
     return f"One for {name}, one for me."
 
 x = input("Enter your name : ")
-# if x: 
-#     pass 
-# else: 
-#     x = "you"
 print(two_fer(x))
