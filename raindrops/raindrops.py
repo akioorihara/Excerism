a8463aacfa28c52b6cb994c88b4d8c1b8d86177d
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-01-01 23:28:49 -0600

    changing inside the function
    Changing the methods inside of this convert function to see if I could pass pytest now but still failing 3 locations

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,15 +1,17 @@
 
 
 def convert(number):
-    if (number % 3):
-        return "Pling"
-    elif (number % 5):
-        return "Plang"
-    elif(number % 7):
-        return "Plong"
-    else:
-        return "not mod by 3, 5, or 7"
+    a = ""
+    if (number % 3 == 0):
+        a += "Pling"
+    if (number % 5 == 0):
+        a += "Plang"
+    if(number % 7 == 0):
+        a += "Plong"
+    if(a is ""):
+        return number
+    
+    return a
      
 x = int(input("Raindrop number: "))
-# xprint(type(x))
 print(convert(x))
