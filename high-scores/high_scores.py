9d6095210e9e75ea204d660325163c303c30a82f
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-01-04 08:29:14 -0600

    completed the top three numbers

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -10,6 +10,7 @@ def personal_best(scores):
 def personal_top_three(scores):
     #sort and then list the last three of the list 
     scores.sort()
+    return scores[-1], scores[-2], scores[-3]
     
 
 
@@ -18,9 +19,9 @@ for i in range(0,9):
     y = (random.randint(1,10))
     scores.insert(i,y) 
     print(scores[i], end=" ")
-print("")
-# x = int(input("Enter a value : "))
-# scores.append(x)
+# print("")
+x = int(input("Enter a value : "))
+scores.append(x)
 print("The late number is : ", latest(scores))
 print("The highest number is : ", personal_best(scores))
 print("The top three numbers are : ", personal_top_three(scores))
