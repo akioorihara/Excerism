5f855c4fea556fdfb44653d3fae32f6af3bde481
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-01-03 20:21:19 -0600

    Return with the best number

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,18 +1,24 @@
-def latest(scores):
-    #append the last val to the end of the list 
-    print(scores[-1])
+import random
+
+def latest(scores): 
+    return scores[-1]
     
-
-
 def personal_best(scores):
-    pass 
+    scores.sort()
+    return scores[-1]
 
 def personal_top_three(scores):
     pass
     #sort and then list the last three of the list 
 
 
-scores = [1,2,3,4,5]
-x = int(input("Enter a value : "))
-scores.append(x)
-latest(scores)
+scores = []
+for i in range(0,9):
+    y = (random.randint(1,10))
+    scores.insert(i,y) 
+    print(scores[i], end=" ")
+print("")
+# x = int(input("Enter a value : "))
+# scores.append(x)
+print("The late number is : ", latest(scores))
+print("The highest number is : ", personal_best(scores))
