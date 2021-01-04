d5e2d54f8296199b3b985621fee5283f2dc91b4a
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-01-03 20:05:01 -0600

    getting the last input of the list

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,10 +1,18 @@
 def latest(scores):
-    pass
+    #append the last val to the end of the list 
+    print(scores[-1])
+    
 
 
 def personal_best(scores):
-    pass
-
+    pass 
 
 def personal_top_three(scores):
     pass
+    #sort and then list the last three of the list 
+
+
+scores = [1,2,3,4,5]
+x = int(input("Enter a value : "))
+scores.append(x)
+latest(scores)
