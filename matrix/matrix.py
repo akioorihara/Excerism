7d869ace2c503dfefb6be56df7ab6900482dacb1
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-01-06 08:05:05 -0600

    Able to pursue and print by 3

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -14,4 +14,4 @@ matrix_string = [9,8,7,5,3,2,6,6,7]
 for i in range(len(matrix_string)): #printing the list using loop 
     print(matrix_string[i], end="")
     if i == 2 or i == 5 or i == 8:
-        print("\n")
+        print(" ")
