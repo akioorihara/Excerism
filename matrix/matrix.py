2bf5157356c7ae05f0bb9a38e9f7af055b6ce029
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-01-06 15:40:33 -0600

    not sure if this is working but adding more lines to methods

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -2,21 +2,24 @@ class Matrix:
     def __init__(self, matrix_string):
         self.matrix_string = matrix_string
 
-    def row(self, index):
-        pass
+    def row(self, index=0):
+        for n in range(1):
+            for i in range(len(matrix_string)):
+                print(matrix_string[i], end="")
+                if i == 2 or i == 5 or i == 8:
+                    print(" ")
 
     def column(self, index):
-        pass
+        for n in range(len(mattrix_string)):
 
 
 matrix_string = [9, 8, 7, 5, 3, 2, 6, 6, 7]
 
-for n in range(1):
-    print("  |----")
-    for i in range(len(matrix_string)):
-        # print(i)  # printing the list using loop
-        if i == 0 or i == 3 or i == 6:
-            print(n, "|", end="")
-        print(matrix_string[i], end="")
-        if i == 2 or i == 5 or i == 8:
-            print(" ")
+# for n in range(1):
+#     for i in range(len(matrix_string)):
+#         print(matrix_string[i], end="")
+#         if i == 2 or i == 5 or i == 8:
+#             print(" ")
+
+x = Matrix
+x.row(0)
