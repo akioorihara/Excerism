48f647a7fb662e4c12686a4b80810358278330d9
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-01-06 08:14:22 -0600

    hardcoded the format for now

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -9,9 +9,14 @@ class Matrix:
         pass
 
 
+matrix_string = [9, 8, 7, 5, 3, 2, 6, 6, 7]
 
-matrix_string = [9,8,7,5,3,2,6,6,7]
-for i in range(len(matrix_string)): #printing the list using loop 
-    print(matrix_string[i], end="")
-    if i == 2 or i == 5 or i == 8:
-        print(" ")
+for n in range(1):
+    print("|----")
+    for i in range(len(matrix_string)):
+        # print(i)  # printing the list using loop
+        if i == 0 or i == 3 or i == 6:
+            print("|", end="")
+        print(matrix_string[i], end="")
+        if i == 2 or i == 5 or i == 8:
+            print(" ")
