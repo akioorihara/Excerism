427a1a196a563a5a0d471607980669afbd508f01
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-01-06 21:13:16 -0600

    looking for suggestion

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -3,23 +3,16 @@ class Matrix:
         self.matrix_string = matrix_string
 
     def row(self, index=0):
-        for n in range(1):
-            for i in range(len(matrix_string)):
-                print(matrix_string[i], end="")
-                if i == 2 or i == 5 or i == 8:
-                    print(" ")
+        for i in range(9):
+            print(matrix_string[i], end="")
+            if i == 2 or i == 5 or i == 8:
+                print(" ")
 
-    def column(self, index):
-        for n in range(len(mattrix_string)):
+    def column(self, index=0):
+        pass
 
 
 matrix_string = [9, 8, 7, 5, 3, 2, 6, 6, 7]
-
-# for n in range(1):
-#     for i in range(len(matrix_string)):
-#         print(matrix_string[i], end="")
-#         if i == 2 or i == 5 or i == 8:
-#             print(" ")
-
 x = Matrix
-x.row(0)
+# x.row(matrix_string)
+x.column(matrix_string)
