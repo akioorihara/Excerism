c09817aef88c7b6627b0a45b5a0b3e7b7b644320
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-01-11 22:18:41 -0600

    raise valueerror
    After adding the valueError, pytest passes all the values

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -2,18 +2,21 @@
 # Haming Distance only calculates equal length 
 
 def distance(strand_a, strand_b):
-    if len(strand_a) != strand_b:
-        raise Exception("Not the same length") 
+    if len(strand_a) != len(strand_b):
+        raise ValueError(" {strand_a} and {strand_b} are not the same length")
     
     #Compare the diff
     diff = 0
-    for i in len(strand_a):
+    for i in range(len(strand_a)):
+    
         if strand_a[i] != strand_b[i]:
-           
-
-
-    #C,A,T, and G 
-        #count the num and prints out 
-
+            diff += 1 
+            # print(diff)
+    
+    return diff
 
 #Test to pass 2 diff strings to compare 
+# Demo 
+strand_a = "GAGCCTACTAACGGGCT"
+strand_b = "CATCGTAATGACGGCCT"
+print(distance(strand_a, strand_b))
