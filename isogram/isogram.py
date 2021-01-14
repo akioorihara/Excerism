feaa739a9c7c902d07f4416087b3a0ac9d3b8437
akioorihara <56176776+akioorihara@users.noreply.github.com>
2021-01-13 22:32:36 -0600

    isogram checkers

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,28 @@
+#isogram checker python 
+
+
+
 def is_isogram(string):
-    pass
+    isogram = 0
+    #check if the value is repated by for-loop 
+    for u in range(len(string)):
+        for i in range(len(string)):
+            if u == i:
+                break
+                #skip this loop and no need to check 
+            if string[u] == string[i]:
+                if ' ' != string[i] or '-' != string[i]:
+                    isogram += 1
+    
+    print(isogram)
+    return isogram
+
+test        = "backgroud"
+testTwo     = "marioandpeach"
+testThree   = "six-year-old"
+testFour    = "six-year-six"
+is_isogram(test)
+is_isogram(testTwo)
+is_isogram(testThree)
+is_isogram(testFour)
+
